cfaa5dc3b38d52c097ea12e2cd6964a8868c5274
Beqael <beqaelashvili3@gmail.com>
2024-01-03 12:19:30 +0400

    modified languages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { PublicLayout } from "@src/layouts/PublicLayout";
+import { FormattedMessage, useIntl } from "react-intl";
+import { LocaleContext } from "./providers/LocaleProvaider/LocaleContext";
+import { useContext } from "react";
+import { LocaleProvider } from "./providers/LocaleProvaider";
 
 const Home = lazy(() => import("@src/views/Home"));
 const Products = lazy(() => import("@src/views/Products"));
 
 function App() {
+  const { toggleLocale } = useContext(LocaleContext);
+  const { formatMessage } = useIntl();
   return (
-    <Suspense fallback={<div>loading</div>}>
-      <Routes>
-        <Route element={<PublicLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <div>
+      {/* <Suspense fallback={<div>loading</div>}>
+        <Routes>
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+          </Route>
+        </Routes>
+      </Suspense> */}
+
+      <div>
+        <button onClick={() => toggleLocale()}>change</button>
+        <FormattedMessage id="hello" defaultMessage={"hello"} />
+      </div>
+    </div>
   );
 }
 
