e21c54576e02e15e2ee484a3e82e054106871827
Beqael <beqaelashvili3@gmail.com>
2024-02-15 19:07:33 +0400

    add_addProductinCart_validation

--- src/hooks/useAddtoCart/useAddinCart.ts
+++ src/hooks/useAddtoCart/useAddinCart.ts
@@ -7,17 +7,26 @@ export function useAddinCart() {
   const { getcartProducts, cartProducts } = useGetCartProducts();
 
   async function addtoCart(productId: string) {
-    const filteredCart = cartProducts?.filter((item) => item.id === productId);
-    console.log(filteredCart);
-
     try {
       setLoading(true);
+      if (
+        cartProducts?.some(
+          (item) => item.cartProduct.id.toString() === productId
+        )
+      ) {
+        alert("Product is already in the cart");
+        return;
+      }
       const resp = await PrivateAxios.post("/cart", {
         product_id: productId,
       });
       await getcartProducts();
     } catch (error) {
-      alert("An attempt to add a product failed");
+      console.error(
+        "An error occurred while adding the product to the cart:",
+        error
+      );
+      alert("An error occurred while adding the product to the cart:");
     } finally {
       setLoading(false);
     }
--- src/hooks/useDeleteCartProduct/useDeleteCartProduct.ts
+++ src/hooks/useDeleteCartProduct/useDeleteCartProduct.ts
@@ -1,7 +1,6 @@
 import { PrivateAxios } from "@src/utils/PriveteAxios";
 import { useState } from "react";
 import { useGetCartProducts } from "../useAddtoCart/getCartProducts";
-import { Divider, Modal } from "antd";
 
 export function useDeleteCartProduct() {
   const [loading, setLoading] = useState(false);
@@ -13,6 +12,7 @@ export function useDeleteCartProduct() {
       const resp = await PrivateAxios.delete(`/cart/${productId}`);
       await getcartProducts();
     } catch (error) {
+      console.error("Failed to delete product", error);
       alert("Failed to delete product");
     } finally {
       setLoading(false);
